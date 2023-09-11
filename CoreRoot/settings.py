24608e0fbc8f86337249af6067d8f8c14425168c
Juwon Hwang <wndnjs719@gmail.com>
2023-09-11 13:33:10 +0900

    생성한 사용자 모델 지정

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'core.user',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core_user.User'
--- core/apps.py
+++ core/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-    label = 'core'
+    name = 'core.user'
+    label = 'core.user'
