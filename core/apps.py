75d6c7165e665dee1e999175b1345d67fe13e6b2
Juwon Hwang <wndnjs719@gmail.com>
2023-09-11 14:15:24 +0900

    apps.py 잘못 입력한 오류 수정 (디렉토리 헷갈림)

--- core/apps.py
+++ core/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.user'
-    label = 'core.user'
+    name = 'core'
+    label = 'core'
