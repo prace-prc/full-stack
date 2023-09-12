0c01e1ecbafd775082ecb90480b04e7d94145a91
Juwon Hwang <wndnjs719@gmail.com>
2023-09-12 14:22:12 +0900

    AbstractViewSet 작성

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from core.abstract.viewsets import AbstractViewSet
 from core.user.serializers import UserSerializer
 from core.user.models import User
 
 
 # Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(AbstractViewSet):
     http_method_names = ('patch', 'get')
     permission_classes = (AllowAny,)
     serializer_class = UserSerializer
