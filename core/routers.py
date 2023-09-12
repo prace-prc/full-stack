aa240254b647d72c4199d3e7bf674b4b48e6e38e
Juwon Hwang <wndnjs719@gmail.com>
2023-09-12 11:27:56 +0900

    토큰 갱신 동작 뷰셋, 라우터에 추가

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 
-from core.auth.viewsets import LoginViewSet
+from core.auth.viewsets import LoginViewSet, RefreshViewSet
 from core.auth.viewsets.register import RegisterViewSet
 from core.user.viewsets import UserViewSet
 
@@ -8,6 +8,7 @@ router = routers.SimpleRouter()
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 urlpatterns = [
     *router.urls,
 ]
