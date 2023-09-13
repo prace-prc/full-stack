d7306e08e65ecf62a905bb90ab7cff18884047d4
Juwon Hwang <wndnjs719@gmail.com>
2023-09-13 13:12:19 +0900

    시리얼라이저에 좋아요 관련 필드 추가

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -9,6 +9,17 @@ from core.user.serializers import UserSerializer
 class PostSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(
         queryset=User.objects.all(), slug_field='public_id')
+    liked = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+
+    def get_liked(self, instance):
+        request = self.context.get('request', None)
+        if request is None or request.user.is_anonymous:
+            return False
+        return request.user.has_liked(instance)
+
+    def get_likes_count(self, instance):
+        return instance.liked_by.count()
 
     def validated_author(self, value):
         if self.context["request"].user != value:
@@ -17,7 +28,7 @@ class PostSerializer(AbstractSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'author', 'body', 'edited', 'created', 'updated']
+        fields = ['id', 'author', 'body', 'edited', 'liked', 'likes_count', 'created', 'updated']
         read_only_fields = ["edited"]
 
     def to_representation(self, instance):
